0c7396ab18c445f769b02c156ac3bf86a8fbf85c
benosolomon <45790644+benosolomon@users.noreply.github.com>
2025-07-01 16:47:29 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,
+    FormsModule,
+    HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'class1';
-
+  name = '';
   ngOnInit(): void {
     console.log("NgOnit checked");
     this.title= "hello";
